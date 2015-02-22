ab2820d9e584f8b9e521a73cb93aa63168469bd0
johnpmitsch <johnpmitsch@gmail.com>
2015-02-22 14:07:18 -0500

    heatmap works!!!!!

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
-from flask import Flask, request, render_template, url_for
-from data import assaults
+from flask import Flask, request, render_template, url_for,jsonify
+from data import assaults, crash_coordinates
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index2.html', assaults = assaults)
+    return render_template('testing.html', assaults=assaults, crashes=crash_coordinates )
 
 if __name__ == '__main__':
     app.run(debug=True)
--- data.py
+++ data.py
@@ -9,10 +9,9 @@ for location in assault_locations:
         lon, lat = ast.literal_eval(location)
         assaults.append((lon, lat))
 
-# crashes = pd.read_csv("crashes.csv")
-# crash_locations = list(crashes['coordinates']) # a list of tuples
-# crash_coordinates = []
-# for location in crash_locations:
-#     if type(location) == float:
-#         lon, lat = ast.literal_eval(location)
-#         crash_coordinates.append((lon, lat))
+crashes = pd.read_csv("crashes.csv")
+crash_locations = list(crashes['coordinates']) # a list of tuples
+crash_coordinates = []
+for location in crash_locations:
+        lon, lat = ast.literal_eval(location)
+        crash_coordinates.append((lon, lat))
