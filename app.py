067deac7a9fde98c410cf5a3dd7ea67977b00616
johnpmitsch <johnpmitsch@gmail.com>
2015-03-12 14:26:06 -0400

    removed unnecessary files

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('testing.html', assaults=assaults, crashes=crash_coordinates )
+    return render_template('index.html', assaults=assaults, crashes=crash_coordinates )
 
 if __name__ == '__main__':
     app.run(debug=True)
